c4bc9ac249d6f13739eaf9fcdb7b6a99f95fea27
Тимофей Цепенников <tnmatn33@gmail.com>
2024-10-25 17:24:06 +0500

    add new buttons and .gitignore

--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,15 @@
-from .models import Bb
+from .models import Bb, Rubric
 from django.shortcuts import render
 
 def index(request):
     bbs = Bb.objects.all()
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+    rubrics = Rubric.objects.all()
+    context = {'bbs':bbs, 'rubrics':rubrics}
+    return render(request, 'bboard/index.html', context)
+
+def rubric_bbs(reqest, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(reqest, 'bboard/rubric_bbs.html', context)
